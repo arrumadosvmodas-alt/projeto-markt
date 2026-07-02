fed8f9f7a93df55a78a2f8e1a6c1a59594edf569
Heitor Lins <arrumadosvmodas@gmail.com>
2026-07-02 08:10:29 -0300

    adicina icones PNG do PWA

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -19,6 +19,9 @@ export default defineConfig({
         icons: [
           { src: "pwa-192.svg", sizes: "192x192", type: "image/svg+xml" },
           { src: "pwa-512.svg", sizes: "512x512", type: "image/svg+xml" },
+          { src: "icon-192.png", sizes: "192x192", type: "image/png" },
+          { src: "icon-512.png", sizes: "512x512", type: "image/png" },
+          { src: "icon-512.png", sizes: "512x512", type: "image/png", purpose: "maskable" },
         ],
       },
     }),
